6abebdd03b2ca3952bdeedd7afb96074adfe185a
AndrasNyiri <nyiriandriss@gmail.com>
2017-11-15 09:39:37 +0100

    merged

--- gittie.py
+++ gittie.py
@@ -1,12 +1,9 @@
 def introduce():
     print("Hello, I'm Gittie!")
-<<<<<<< HEAD
 def add(a, b):
     return a+b
-=======
 
 
 def joke():
     print("Vigyázz kisegér, rádesik a vaj!")
     print("Ráma margarin?")
->>>>>>> 924019a37c6baeea7d4846f1806820b8f403cbf4
